e22e358c5d199ab0e6a4b5a9d1a512f6b4b4d28c
Felipe M3nin0 <felipe.carlos@fatec.sp.gov.br>
2018-10-28 11:33:47 -0300

    Adição do scrap dos arquivos analisados

---
+++ recuperacao_dados/index.js
@@ -0,0 +1,35 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const download = require('download-file')
+
+const options = {
+  uri: 'http://dados.gov.br/dataset/sistema-nacional-de-estatisticas-de-seguranca-publica',
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
+
+rp(options).then(($) => {
+	$('.resource-url-analytics').each(function() {
+		let url = $(this).attr('href');
+
+		// Filtrando o nome do arquivo
+		let listNames = url.split('/'); 
+		let filename = listNames[listNames.length - 1];
+
+		// Baixando o arquivo
+		download(url, {
+			directory: '../data/',
+			'filename': filename
+		}, function(err) {
+			if (err) {
+				throw err
+			} else {
+				console.log('Arquivo', filename, ' foi baixado com sucesso!');
+			}
+		});
+
+	});
+}).catch((err) => {
+	console.log(err);
+});
