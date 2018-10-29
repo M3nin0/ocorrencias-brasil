73467a77460686d958cfe8113d78a7d8dc7063b6
Phelps <phelpsmemo@gmail.com>
2018-10-28 21:41:48 -0300

    Atualização das funções de carregamento dos dados

--- src/data_utils.R
+++ src/data_utils.R
@@ -5,13 +5,14 @@ Script para a leitura, limpeza e organização dos dados utilizados
 #' Função para a leitura de todos os dados do diretório `data`
 #' @return data.frame
 readAllData <- function(){
-  files <- dir('../data/');
+  path <- '../dados/';
+  files <- dir(path);
   allData <- data.frame();
   
   for(f in files) {
     if (!grepl('.pdf', f)) {
       print(f);
-      data <- read.csv(paste('../data/', f, sep = ''), skip = 4, sep = ';');
+      data <- read.csv(paste(path, f, sep = ''), skip = 4, sep = ';');
       allData <- rbind(allData, data);
     }
   }
@@ -21,11 +22,12 @@ readAllData <- function(){
 #' Função para a leitura de um ano específico de ocorrências
 #' @return data.frame
 readSpecificYear <- function(year) {
-  files <- dir('../data/');
+  path <- '../dados/';
+  files <- dir(path);
   
   for (f in files) {
     if (grepl(toString(year), f)) {
-      return(read.csv(paste('../data/', f, sep = ''), skip = 4, sep = ';'));
+      return(read.csv(paste(path, f, sep = ''), skip = 4, sep = ';'));
     } 
   }
 }
