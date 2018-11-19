49b68d02ca9abd496afb5e1aa29b87bebd304d5c
weslei luiz <weslei.paula@fatec.sp.gov.br>
2018-11-19 12:59:00 -0200

    correção nas labels x

--- tool/python/data_utils.py
+++ tool/python/data_utils.py
@@ -30,3 +30,23 @@ def read_all_data():
         data = pd.read_csv(file, encoding='latin-1',sep=';',skiprows=4)
         years.append(data)
     return format_columns(pd.concat(years))
+
+def format_legend(legend_handless):
+    legend = []
+    for label in legend_handless[1]:
+            legend.append(label.split(",")[1][:-1])
+    return legend
+
+def create_month_xlegend(months):
+    xlegend = ['']
+    for month in months:
+        if(int(month[1].split('/')[0]) % 2 != 0):
+            xlegend.append(month[1])
+    return xlegend
+
+def create_year_xlegend(years):
+    xlegend = ['']
+    for year in years:
+        if(int(year) % 2 == 0):
+            xlegend.append(year)
+    return xlegend
